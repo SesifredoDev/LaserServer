5c26ef75738b9a56ca0861092d990f16ef67b621
SesifredoDev <93771023+SesifredoDev@users.noreply.github.com>
2024-08-13 11:48:28 +0100

    update

--- main.js
+++ main.js
@@ -8,24 +8,19 @@ const server = require('http').createServer(
     }
 );
 const heatGen = require('./heatmap')
-
-const turf = require('@turf/turf');
-const geocluster = require('geocluster');
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceKey.json');
 
-var points = [];
-var heatValues = []
 var leaderboard = [];
 
 defaultHp = 100;
 
 
-
+const rooms = {};
 
 app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  
+    credential: admin.credential.cert(serviceAccount),
+
 });
 
 
@@ -39,118 +34,197 @@ const io = require('socket.io')(server, {
     }
 })
 
-getData = async () =>{
-    const query = await db.collection("scores").get().then(res =>{
+getData = async () => {
+    const query = await db.collection("scores").get().then(res => {
         console.log(res)
     })
 }
 
-const query = db.collection('scores').orderBy('score', 'desc').limit(10)
 
-const observer = query.onSnapshot( async querySnapshot => {
-    let resultArray = []
-    querySnap =  querySnapshot.docs.map( async (d)=> {
-        const mainObj = d.data()
-        if(mainObj.userId.path){
-            const userRef = db.doc(mainObj.userId?.path)
-            const userDoc = await userRef.get()
-            userName = userDoc.data().name
-            let result= {
-                name: userName ,
-                score: mainObj.score
+const listenToLeaderboardChanges = () => {
+    db.collection('games').onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+            if (change.type === 'modified') {
+                const updatedDocument = change.doc.data();
+
+                // Retrieve gameCode and leaderboard from the document
+                const { gameCode, leaderboard } = updatedDocument;
+
+                if (leaderboard && gameCode) {
+                    console.log(`Leaderboard updated for gameCode ${gameCode}:`, leaderboard);
+                    // console.log(rooms[gameCode])
+                    rooms[gameCode].leaderboard = leaderboard;
+
+                    // Emit the leaderboard update to the specific room associated with gameCode
+                    io.to(gameCode).emit('leadUpdate', {leaderboard: leaderboard});
+                }
             }
-            resultArray.push(result)
-        }
-  }, err => {
-    console.log(`Encountered error: ${err}`);
-  });
-  Promise.all(querySnap).then(function() {
-    leaderboard = resultArray
-    io.emit("leadUpdate", {leaderboard: leaderboard})
-    console.log(leaderboard)
-  })
-})
+        });
+    });
+};
+
+async function loadGamesIntoRooms() {
+    try {
+        const gamesSnapshot = await admin.firestore().collection('games').get();
+
+        gamesSnapshot.forEach(gameDoc => {
+            const gameData = gameDoc.data();
+
+            // Add game data to rooms object, keyed by gameId
+            rooms[gameData.gameCode] = {
+                gameCode: gameData.gameCode,
+                name: gameData.name,
+                rules: gameData.rules,
+                leaderboard: gameData.leaderboard,
+                owner: gameData.owner,
+                heatValues: [],
+                points: []
+            };
+
+            console.log(`Loaded game: ${gameData.gameCode}`);
+        });
+
+        console.log('All games loaded into rooms object:', rooms);
+    } catch (error) {
+        console.error('Error loading games:', error);
+    }
+}
+
+// Execute the function
+loadGamesIntoRooms();
+listenToLeaderboardChanges();
+
+
 
 
 io.on('connection', client => {
     const id = client.id;
     let uid = null;
+    let gameCode = null;
     // console.log(client)3
     client.emit("connectedNewUser", {})
     client.on("userData", (data) => {
         uid = data;
     })
-    client.on("setUpGun", (data)=>{
 
+    client.on("createGame", (data) => {
+        console.log(data)
+        gameObj = {
+            gameCode: data.gameCode,
+            name: data.name,
+            rules: data.rules,
+            leaderboard: [],
+            owner: data.owner,
+        }
+        admin.firestore().collection('games').add(gameObj);
+        gameObj.points = [];
+        gameObj.heatValues = [];
+        rooms[data.gameCode] = gameObj;
+
+        listenToLeaderboardChanges(data.gameCode);
+
+        console.log(`Game created: ${data.gameCode}`);
     })
 
+    client.on('joinGame', async (data) => {
+        gameCode = data.gameCode;
+        client.join(gameCode);
+        console.log(`User ${uid} joined game ${gameCode}`);
+        let playerInfo = data.playerInfo;
+        const found = rooms[gameCode].leaderboard.find((item) => item.email == playerInfo.email);
+        if (!found) {
+            let newHex = generateUniqueHex(rooms[gameCode].leaderboard, 2);
+            playerInfo.gunCode = newHex;
+            playerInfo.currentScore = 0;
+            rooms[gameCode].leaderboard.push(playerInfo);
+            const querySnapshot = await db.collection('games').where('gameCode', '==', gameCode).get();
+            querySnapshot.forEach(async (doc) => {
+                await db.collection('games').doc(doc.id)
+                    .update({ 'leaderboard': rooms[gameCode].leaderboard });
+            })
+            
+            io.to(id).emit("connectGame", {code: gameCode, leaderboard: rooms[gameCode].leaderboard });
+            io.to(id).emit("gunCodeUpdate", newHex);
+        } else {
+            
+            io.to(id).emit("connectGame", {code: gameCode, leaderboard: rooms[gameCode].leaderboard });
+            io.to(id).emit("gunCodeUpdate", found.gunCode);
+        }
+
+
+
+    })
 
 
 
     client.on('gotshot', async function (data) {
         let newShot = [data.data.location.latitude, data.data.location.longitude, new Date().getMilliseconds()]
-        
-        var query = await admin.firestore().collection('scores')
-            .where('gunCode', '==', data.data.id)
+        console.log(newShot);
+        var query = await admin.firestore().collection('games')
+            .where('gameCode', '==', gameCode)
             .get().then(async result => {
-                let snapshot = result.docs[0];
-                const documentRef = snapshot.ref
-                let currentScore = snapshot.data().score
-                let newScore = 100
-                if(currentScore !== undefined){  
-                     newScore += currentScore;
-                }
-                documentRef.update({ 'score': newScore })
-                // var userDoc = (await admin.firestore().collection('users').doc(id).get()).data()
-                // let damage = Math.floor(Math.random() * (4 - 2 + 1) + 2);
-                // if(userDoc.hp !== undefined){
-                //     damage =  userDoc.hp - damage;
-                // }else{
-                //     damage = defaultHp - damage;
-                // }
-                // userDoc.update({ 'hp': damage })
-                // io.to(id).emit("takeDamage", {damage: damage})
-                // Heat Map Fun
-        setTimeout(() => {
+                    if(!result.empty) {
+                        let snapshot = result.docs[0];
+                    const documentRef = snapshot.ref;
+                    let leaderboard = snapshot.data().leaderboard;
+                    let playerIndex = leaderboard.findIndex(player => player.gunCode == data.data.id);
+                    let newScore = 100
+                    if (leaderboard[playerIndex+1]?.currentScore !== undefined) {
+                        newScore += leaderboard[playerIndex+1].currentScore;
+                    }
 
-            heatValues.push(newShot)
+                    leaderboard[playerIndex+1].currentScore = newScore;
+                    documentRef.update({ 'leaderboard': leaderboard });
 
+                    io.to(gameCode).emit("leadUpdate", {leaderboard: leaderboard});
 
-            newPoints = generateHeatMap(heatValues);
+                    setTimeout(() => {
 
-            if (!compareArrays(points, newPoints)) {
-                points = newPoints;
-                io.emit("heatUpdate", points);
-            }
+                        rooms[gameCode].heatValues.push(newShot)
 
-            setTimeout(() => {
-                heatValues = heatValues.filter(item => item != newShot)
-                newPoints = generateHeatMap(heatValues);
-                if (!compareArrays(points, newPoints)) {
-                    points = newPoints;
-                    io.emit("heatUpdate", points);
-                }
+                        
+                        newPoints = generateHeatMap(rooms[gameCode].heatValues);
 
-            }, 10000)
-        }, 10000)
+                        if (!compareArrays(rooms[gameCode].points, newPoints)) {
+                            rooms[gameCode].points = newPoints;
+                            
+                            console.log(rooms[gameCode].points, gameCode)
+                            io.to(gameCode).emit("heatUpdate", rooms[gameCode].points);
+                        }
 
+                        setTimeout(() => {
+                            rooms[gameCode].heatValues = rooms[gameCode].heatValues.filter(item => item != newShot)
+                            newPoints = generateHeatMap(rooms[gameCode].heatValues);
+                            if (!compareArrays(rooms[gameCode].points, newPoints)) {
+                                rooms[gameCode].points = newPoints;
+                                io.to(gameCode).emit("heatUpdate", rooms[gameCode].points);
+                            }
+
+                        }, 10000)
+                    }, 10000)
+                    }
+                    
+                
             });
-        
-        
+
+
 
     })
     client.on('getHeat', (data) => {
-        io.to(id).emit("heatUpdate", points)
+        io.to(id).emit("heatUpdate", rooms[gameCode]?.points)
 
     })
     client.on('getLead', (data) => {
-        io.to(id).emit("leadUpdate", {leaderboard: leaderboard})
+        io.to(id).emit("leadUpdate", { leaderboard: rooms[gameCode]?.leaderboard })
 
     })
 
     client.on('disconnect', () => {
         console.log("client disconnected");
     })
+
+
+
 
 
 })
@@ -230,3 +304,28 @@ function generateHeatMap(points, distanceThreshold = 10) {
 const compareArrays = (a, b) => {
     return JSON.stringify(a) === JSON.stringify(b);
 };
+
+
+// Function to generate a random hex value
+function generateRandomHex(length) {
+    let hex = '';
+    const characters = '0123456789ABCDEF';
+    for (let i = 0; i < length; i++) {
+        hex += characters.charAt(Math.floor(Math.random() * 16));
+    }
+    return hex;
+}
+
+// Function to check if a hex value already exists in the array
+function hexExistsInArray(array, hex) {
+    return array.some(item => item.hex === hex);
+}
+
+// Function to generate a unique hex value
+function generateUniqueHex(array, length) {
+    let uniqueHex;
+    do {
+        uniqueHex = generateRandomHex(length);
+    } while (hexExistsInArray(array, uniqueHex));
+    return uniqueHex;
+}
